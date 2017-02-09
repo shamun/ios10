f91e53dfc014f09c07e3755fcd4e8b1654383ffd
Shamun <shamun.toha@gmail.com>
2017-02-09 17:41:10 +0100

    Full submit

---
+++ SMS/IOS8SwiftSendSMSTutorial/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  IOS8SwiftSendSMSTutorial
+//
+//  Created by Arthur Knopper on 23/11/14.
+//  Copyright (c) 2014 Arthur Knopper. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+  @IBAction func sendMessage(_ sender: AnyObject) {
+    let messageVC = MFMessageComposeViewController()
+    
+    messageVC.body = "Enter a message";
+    messageVC.recipients = ["Enter tel-nr"]
+    messageVC.messageComposeDelegate = self;
+    
+    self.present(messageVC, animated: false, completion: nil)
+  }
+  
+  func messageComposeViewController(_ controller: MFMessageComposeViewController!, didFinishWith result: MessageComposeResult) {
+    
+    /*
+    switch (result) {
+    case MessageComposeResult.cancelled.value:
+      print("Message was cancelled")
+      self.dismiss(animated: true, completion: nil)
+    case MessageComposeResult.failed.value:
+      print("Message failed")
+      self.dismiss(animated: true, completion: nil)
+    case MessageComposeResult.sent.value:
+      print("Message was sent")
+     self.dismiss(animated: true, completion: nil)
+    default:
+      break;
+    }*/
+    
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
